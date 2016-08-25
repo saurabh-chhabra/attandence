6b91b52dd137832905b4f7f9e8c2a84529b7c068
akhilesh <akhilesingh00@gmail.com>
2016-08-25 17:24:31 +0530

    create the login page

--- views/site/login.php
+++ views/site/login.php
@@ -7,36 +7,23 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Login';
+$this->title = 'Attandence Login';
 
 ?>
 <main id="content" class="widget-login-container" role="main">
     <div class="row">
         <div class="col-lg-4 col-sm-6 col-xs-10 col-lg-offset-4 col-sm-offset-3 col-xs-offset-1">
-            <h4 class="widget-login-logo animated fadeInUp">
-                <i class="fa fa-circle text-gray"></i>
-                sing
-                <i class="fa fa-circle text-warning"></i>
-            </h4>
             <section class="widget widget-login animated fadeInUp">
                 <header>
-                    <h3>Login to your Sing App</h3>
+                    <h3>Login to your Dashboard</h3>
                 </header>
                 <div class="widget-body">
-                    <p class="widget-login-info">
-                        Use Facebook, Twitter or your email to sign in.
-                    </p>
-                    <p class="widget-login-info">
-                        Don't have an account? Sign up now!
-                    </p>
-
-
-                        <?php $form = ActiveForm::begin([
+                    <?php $form = ActiveForm::begin([
                             'id' => 'login-form',
-                            'options' => ['class' => 'form-horizontal'],
+                            'options' => ['class' => 'form-horizontal mt-lg'],
                             'fieldConfig' => [
-                                'template' => "{label}<div class=\"col-lg-10\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-                                'labelOptions' => ['class' => 'col-lg-1 control-label'],
+                                'template' => "{label}<div class=\"col-lg-9\">{input}</div>\n<div class=\"col-lg-offset-3 col-lg-9\">{error}</div>",
+                                'labelOptions' => ['class' => 'col-lg-3 control-label'],
                             ],
                         ]); ?>
 
@@ -45,7 +32,7 @@ $this->title = 'Login';
                         <?= $form->field($model, 'password')->passwordInput() ?>
 
                         <?= $form->field($model, 'rememberMe')->checkbox([
-                            'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+                            'template' => "<div class=\"col-lg-offset-1 col-lg-9\">{input} {label}</div>\n<div class=\"col-lg-offset-3  col-lg-9\">{error}</div>",
                         ]) ?>
 
                         <div class="form-group">
@@ -55,8 +42,6 @@ $this->title = 'Login';
                         </div>
 
                         <?php ActiveForm::end(); ?>
-
-
                 </div>
             </section>
         </div>
