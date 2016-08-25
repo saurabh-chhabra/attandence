ac7b4ada7d1497f1bcec55bbbfe3e18c9d84b965
akhilesh <akhilesingh00@gmail.com>
2016-08-25 15:13:12 +0530

    configure the database for the connection

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=attandence',
     'username' => 'root',
-    'password' => '',
+    'password' => 'acme123',
     'charset' => 'utf8',
 ];
